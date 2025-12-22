d1b1740c7d42091de1d6b600d854b1256ca4ac29
Najad <mrnajadas@gmail.com>
2025-12-22 21:09:51 +0530

    basic calculator is done

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+// functions that evaluate add,sub,mul,div
 function add(num1,num2){
     return num1+num2;
 }
@@ -15,3 +18,64 @@ function div(num1,num2){
     }
     return num1/num2;
 }
+
+
+// input variables for num1,operator,num2 
+
+let num1=0;
+let num2="";
+let opVariable=null;
+
+
+// function that operate the calculator
+function operate(num1,operator,num2){
+    if (operator=="+"){
+        return add(num1,num2);
+    }
+    else if(operator=="-"){
+        return sub(num1,num2)
+    }
+    else if(operator=="*"){
+        return mul(num1,num2);
+    }
+    else if(operator=="/"){
+        return div(num1,num2); 
+    }
+}
+
+//function to populate in the display and storing in a variable when a digit button is clicked
+function populate(){
+    const digits = document.querySelectorAll(".digit");
+    const display = document.querySelector(".display");
+
+
+
+    digits.forEach(digit => {
+    digit.addEventListener("click", () => {
+        display.value += (digit.textContent);
+        num2 = Number(display.value);
+        
+
+    });
+    });
+}
+
+populate();
+
+const operator = document.querySelectorAll(".operator");
+const display = document.querySelector(".display");
+
+operator.forEach(op =>{
+    op.addEventListener("click",()=>{
+        num1 = num2;
+        opVariable = op.textContent;
+        display.value = "";
+    })
+})
+
+
+const equals = document.querySelector(".equals");
+equals.addEventListener("click",()=>{
+    const result = operate(num1,opVariable,num2);
+    display.value = result;
+})
