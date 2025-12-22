5c81da731871ff6aefa13c62a7d8383a2a1906d0
Najad <mrnajadas@gmail.com>
2025-12-22 19:46:54 +0530

    gave a favicon to the website

---
+++ script.js
@@ -0,0 +1,16 @@
+function add(num1,num2){
+    return num1,num2;
+}
+
+function sub(num1,num2){
+    return num1-num2;
+}
+function mul(num1,num2){
+    return num1*num2;
+}
+function div(num1,num2){
+    if (num2===0){
+        alert("FUCK YOU MFS,ARE YOU TRYING TO CRASH MY CALCULATOR??");
+    }
+    return num1/num2;
+}
